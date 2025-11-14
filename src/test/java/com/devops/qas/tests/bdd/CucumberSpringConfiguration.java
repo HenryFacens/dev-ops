faa8b60a44bc14a02c47fc8a437f748fcc1f4907
Henry S Almeida <Henry.Almeida@ext.rumolog.com>
2025-11-13 21:09:39 -0300

    d

--- src/test/java/com/devops/qas/tests/bdd/CucumberSpringConfiguration.java
+++ src/test/java/com/devops/qas/tests/bdd/CucumberSpringConfiguration.java
@@ -3,13 +3,7 @@ package com.devops.qas.tests.bdd;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-/**
- * Configuração do contexto Spring para testes Cucumber BDD.
- * Esta classe conecta o Cucumber ao contexto do Spring Boot.
- */
 @CucumberContextConfiguration
 @SpringBootTest
 public class CucumberSpringConfiguration {
-    // Esta classe não precisa de implementação
-    // Ela apenas fornece a configuração necessária para o Cucumber usar o Spring
 }
